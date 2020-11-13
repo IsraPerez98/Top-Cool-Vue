446ed52d4d44293cb40fd6d13aff70a6899d1f54
IsraPerez98 <isra1998isra@gmail.com>
2020-11-13 16:12:14 -0300

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
