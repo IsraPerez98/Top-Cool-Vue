aa97381870aad0eb298695952379b65a2967b33f
IsraPerez98 <isra1998isra@gmail.com>
2020-11-13 17:03:52 -0300

    font awesome

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import App from './App.vue'
 import router from './router'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faUserSecret)
+library.add(faFontAwesome)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
