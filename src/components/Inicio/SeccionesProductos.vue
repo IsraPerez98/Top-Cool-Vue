cce22a44262aa9717c3f51786e368844cd996203
Israel Perez <isra1998isra@gmail.com>
2020-11-18 19:10:49 -0300

    secciones de productos sin props

--- src/components/Inicio/SeccionesProductos.vue
+++ src/components/Inicio/SeccionesProductos.vue
@@ -22,8 +22,23 @@ export default {
   components: {
       SeccionProductos,
   },
-  props : {
-      seccionesDisponibles: Array,
+  data: () => {
+    return {
+      seccionesDisponibles: [
+        {
+            nombreSeccion: "ola",
+            imagenSeccion: "https://www.concierto.cl/wp-content/uploads/2020/01/huevito-rey.jpg",
+        },
+        {
+            nombreSeccion: "adios xd",
+            imagenSeccion: "https://img.discogs.com/qfFnZ6C7Q4_A9SQXFdqHvHxM3wQ=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/L-1729366-1600126773-5014.jpeg.jpg",
+        },
+        {
+            nombreSeccion: "que pasa larva xd",
+            imagenSeccion: "https://i.ytimg.com/vi/bNq_wjvwb0M/hqdefault.jpg",
+        },
+      ]
+    }
   }
 }
 </script>
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -2,9 +2,7 @@
   <div class="inicio">
     <b-container class="secciones-productos">
         <CarruselDestacados />
-        <SeccionesProductos 
-            v-bind:seccionesDisponibles="seccionesDisponibles"
-        />
+        <SeccionesProductos />
     </b-container>
   </div>
 </template>
@@ -21,23 +19,6 @@ export default {
       SeccionesProductos,
       CarruselDestacados,
   },
-  data: () => {
-    return {
-      seccionesDisponibles: [
-        {
-            nombreSeccion: "ola",
-            imagenSeccion: "https://www.concierto.cl/wp-content/uploads/2020/01/huevito-rey.jpg",
-        },
-        {
-            nombreSeccion: "adios xd",
-            imagenSeccion: "https://img.discogs.com/qfFnZ6C7Q4_A9SQXFdqHvHxM3wQ=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/L-1729366-1600126773-5014.jpeg.jpg",
-        },
-        {
-            nombreSeccion: "que pasa larva xd",
-            imagenSeccion: "https://i.ytimg.com/vi/bNq_wjvwb0M/hqdefault.jpg",
-        },
-      ]
-    }
-  }
+  
 }
 </script>
