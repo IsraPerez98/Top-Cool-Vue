5e32ae9adae302815920742f60be028b36975da0
Israel Perez <isra1998isra@gmail.com>
2020-11-18 19:09:25 -0300

    carrusel destacados

---
+++ src/components/Inicio/CarruselDestacados.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="productos-destacados">
+      <h2>Productos destacados de la semana</h2>
+      <b-carousel
+      id="carrusel-destacados"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="10"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+      >
+      <b-carousel-slide v-for="(producto, index) in productos_destacados" v-bind:key="index"
+        v-bind:caption="producto.nombreProducto"
+        v-bind:text="producto.descripcionProducto"
+        v-bind:img-src="producto.imagenProducto"
+      >
+      </b-carousel-slide>
+
+      </b-carousel>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'CarruselDestacados',
+  data: () => {
+    return {
+      productos_destacados: [
+        {
+            nombreProducto: "ola",
+            descripcionProducto: "xde",
+            imagenProducto: "https://talcans.cl/uploads/file_1528057544.png",
+        },
+        {
+            nombreProducto: "adsfadfs",
+            descripcionProducto: "xde",
+            imagenProducto: "https://www.eldinamo.cl/wp-content/uploads/2016/03/CHOCHE.jpg",
+        },
+        {
+            nombreProducto: "adsfadfs",
+            descripcionProducto: "xde",
+            imagenProducto: "https://media.biobiochile.cl/wp-content/uploads/2020/01/captura.jpeg",
+        },
+      ]
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="inicio">
     <b-container class="secciones-productos">
+        <CarruselDestacados />
         <SeccionesProductos 
             v-bind:seccionesDisponibles="seccionesDisponibles"
         />
@@ -12,11 +13,13 @@
 
 // @ is an alias to /src
 import SeccionesProductos from '@/components/Inicio/SeccionesProductos.vue'
+import CarruselDestacados from '@/components/Inicio/CarruselDestacados.vue'
 
 export default {
   name: 'Inicio',
   components: {
       SeccionesProductos,
+      CarruselDestacados,
   },
   data: () => {
     return {
