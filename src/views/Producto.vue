2400a90791c7815ce1a5ea0f28fcb7f512142e81
Israel Perez <isra1998isra@gmail.com>
2020-11-19 18:23:22 -0300

    pagina productos

--- src/views/Producto.vue
+++ src/views/Producto.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="producto">
-      <h1>{{$route.params.id}}</h1>
-      <b-container>
-        <b-img v-bind:src="imagenesProducto[imagenSeleccionada]"></b-img>
+  <b-container fluid="xl" class="producto">
+    <b-row>
+      <b-col class="imagenes-producto">
+        <b-img class="imagen-grande" v-bind:src="imagenesProducto[imagenSeleccionada]"></b-img>
         
-        <b-container>
-            <b-img class="imagenes-pequenas" thumbnail v-for="(imagen, index) in imagenesProducto" v-bind:key="index"
+        <b-container class="imagenes-thumbnails">
+            <b-img class="imagen-thumbnail" v-on:click="imagenSeleccionada=index" thumbnail v-for="(imagen, index) in imagenesProducto" v-bind:key="index"
                 width="100"
                 height="100"
                 v-bind:src="imagen"
             >
             </b-img>
         </b-container>
-      </b-container>
-  </div>
+      </b-col>
+
+      <b-col class="texto-producto">
+        <h1 class="nombre-producto">{{nombreProducto}}</h1>
+        <p class="descripcion-producto">{{descripcionProducto}}</p>
+        <p class="precio-producto">Precio: ${{precioProducto}}</p>
+      </b-col>
+    </b-row>
+    <b-container class="botones">
+      <b-button variant="outline-primary">Deja tu Comentario</b-button>
+      <b-button variant="success">Agregar al Carro</b-button>
+    </b-container>
+  </b-container>
 </template>
 
 <script>
@@ -41,5 +52,38 @@ export default {
 </script>
 
 <style scoped lang="scss">
+  .producto {
+    .imagenes-producto {
+      margin-top: 10px;
+      margin-bottom: 10px;
+      
+      .imagenes-thumbnails {
+        background-color: #C4C4C4;
+        max-width: 60%;
+        padding: 5px;
+        margin-top: 10px;
+        margin-bottom: 10px;
 
+        .imagen-thumbnail {
+          cursor: pointer;
+        }
+      }
+    }
+    .texto-producto {
+      background-color: #C4C4C4;
+      margin-top: 10px;
+      margin-bottom: 10px;
+      padding: 10px;
+
+      .precio-producto {
+        margin-top: auto;
+      }
+    }
+
+    .botones {
+      button {
+        margin: 20px;
+      }
+    }
+  }
 </style>
