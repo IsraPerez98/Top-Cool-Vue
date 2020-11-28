fec5f73cbcc6c7307b3a913f9a42c27fbf335226
IsraPerez98 <isra1998isra@gmail.com>
2020-11-27 22:34:47 -0300

    pagina de productos terminada

--- src/components/Productos/Producto.vue
+++ src/components/Productos/Producto.vue
@@ -1,25 +1,36 @@
 <template>
   <div class="producto">
-      <b-card
-        v-bind:title="nombre"
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
-        <b-card-text>
-            {{descripcion}}
-        </b-card-text>
-        <b-card-text>
-            Tallas Disponibles: 
-            <ul style="list-style-type:none;">
-                <li v-for="(talla, index) in tallas" v-bind:key="index">
-                    {{talla.toUpperCase()}}
-                </li>
-            </ul>
-        </b-card-text>
-        <b-card-text>
-            <p>Precio: {{precio}}</p>
-        </b-card-text>
-      </b-card>
+    <b-container class="bg-secondary" >
+      <b-row align-h="between">
+        <b-column>
+          <ul id="imagenes" style="list-style-type:none; padding:0;">
+            <li v-for="(imagen, index) in imagenes" v-bind:key="index">
+              <b-img height="200px" v-bind:alt="nombre" v-bind:src="imagen"></b-img>
+            </li>
+          </ul>
+        </b-column>
+        <b-column style="width: 50%;">
+          <h3>{{nombre}}</h3>
+          <p class="descripcion">{{descripcion}}</p>
+          <p class="tallas">Tallas: </p>
+          <ul class="tallas" id="tallas" style="list-style-type:none; padding:0;">
+            <li v-for="(talla, index) in tallas" v-bind:key="index">
+              {{talla.toUpperCase()}}
+            </li>
+          </ul>
+        </b-column>
+        <b-column>
+          <ul class="precios-especiales" id="preciosEspeciales" style="list-style-type:none; padding:0;">
+            <li v-for="(data, index) in preciosEspeciales" v-bind:key="index">
+              <h3 v-for="(precio, texto) in data" v-bind:key="texto">
+                {{texto}} : ${{precio}}
+              </h3>
+            </li>
+          </ul>
+          <h3>Precio: {{precio}}</h3>
+        </b-column>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -29,13 +40,46 @@ export default {
   props: {
     nombre: String,
     descripcion: String,
+    imagenes: Array,
     tallas: Array,
     precio: Number,
+    preciosEspeciales: Array, // [{"Por Mayor": 5900}, {etc}]
   }
 }
+
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+  .producto {
+    margin: auto;
+    width: 90%;
+    margin-bottom: 20px;
+    margin-top: 20px;
 
+    .container {
+      padding: 20px;
+      border-radius: 12px;
+
+      color: white;
+      text-align: center;
+
+      b-column {
+        margin: auto;
+        padding-top: 10px;
+        padding-bottom: 10px;
+
+        .tallas {
+          font-size: 18px;
+          font-weight: bold;
+          li {
+            display: inline;
+            margin: auto;
+          }
+        }
+      }
+    }
+  }
 </style>
--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -5,8 +5,10 @@
         <Producto
           v-bind:nombre="producto.nombre"
           v-bind:descripcion="producto.descripcion"
+          v-bind:imagenes="producto.imagenes"
           v-bind:tallas="producto.tallas"
           v-bind:precio="producto.precio"
+          v-bind:preciosEspeciales="producto.preciosEspeciales"
         />
       </li>
     </ul>
@@ -17,16 +19,29 @@
 // @ is an alias to /src
 import Producto from '@/components/Productos/Producto.vue'
 
+import imagenAbrigoAnimalPrint from '@/assets/imgs/Productos/AbrigoAnimalPrint.png'
+import imagenMascarillas from '@/assets/imgs/Productos/Mascarillas.png'
+
 export default {
   name: 'Productos',
   data: () => {
     return {
       productos: [
         {
-          nombre: "Aaaa",
-          descripcion: "xd",
-          tallas: ["xl", "l"],
+          nombre: "Abrigo animal print",
+          descripcion: "Descripcion no disponible",
+          imagenes: [imagenAbrigoAnimalPrint,],
+          tallas: ["s", "m", "l", "xl"],
+          precio: 7990,
+          preciosEspeciales: [{"Por Mayor": 5990},],
+        },
+        {
+          nombre: "Mascarillas distintos colores y diseños",
+          descripcion: "Forradas en lycra polyamyda elastano una tela antitranspirante y antibacterial de secado rápido y por fuera son de tela porosa opaca.  Pasan la prueba del spray",
+          imagenes: [imagenMascarillas,],
+          tallas: ["s","m","l","xl"],
           precio: 3000,
+          preciosEspeciales: [],
         },
       ]
     }
@@ -36,3 +51,10 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+  .productos {
+    padding-top: 30px;
+  }
+  
+</style>
