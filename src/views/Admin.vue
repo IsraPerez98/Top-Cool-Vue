ade2e5e6ef5f079e415878aacfb5faa4fb8fbde9
IsraPerez98 <isra1998isra@gmail.com>
2020-12-12 20:56:14 -0300

    panel solo administradores

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,11 +1,23 @@
 <template>
     <div class="admin-panel">
+        <div class="no-admin" v-if="!(usuario_admin)">
+            <h1>Este panel es solo para Administradores</h1>
+            <router-link class="nav-link" active-class="active" to="/" exact>
+                <b-button variant="primary">Volver a Inicio</b-button>
+            </router-link>
+        </div>
+        <div class="admin" v-else>
+
+        </div>
     </div>
 </template>
 
 <script>
 export default {
   name: "Admin",
+  props: {
+      usuario_admin: Boolean,
+  }
 };
 </script>
 
