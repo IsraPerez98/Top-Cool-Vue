b489f5073a7b5138b026d734f780921c1ef1f4fd
IsraPerez98 <isra1998isra@gmail.com>
2020-12-19 18:11:50 -0500

    seleccion de tallas

--- src/views/Producto.vue
+++ src/views/Producto.vue
@@ -19,7 +19,7 @@
         <h1 class="nombre-producto">{{dataProducto.nombre}}</h1>
         <p class="descripcion-producto">{{dataProducto.descripcion}}</p>
         <p class="info-extra">{{infoExtra}}</p>
-        <p class="tallas-producto-titulo">Tallas: </p>
+        <p class="tallas-producto-titulo">Tallas Disponibles: </p>
         <ul class="tallas-producto" id="dataProducto.tallas" style="list-style-type:none; padding:0;">
           <li v-for="(talla, index) in dataProducto.tallas" v-bind:key="index" v-on:click="seleccionarTalla(talla)">
             <button class="boton-talla seleccionada" v-if="dataProducto.tallaSeleccionada === talla">{{talla.toUpperCase()}}</button>
@@ -37,7 +37,7 @@
       </b-col>
     </b-row>
     <b-container class="botones">
-      <b-button variant="outline-primary">Deja tu Comentario</b-button>
+      <b-button variant="outline-primary">Deja tu Comentario</b-button> 
       <b-button variant="success" v-on:click="agregarACarrito" >Agregar al Carro</b-button>
     </b-container>
     <div class="alert alert-primary" role="alert" v-if="enCarro > 0">
@@ -86,7 +86,7 @@ export default {
 
     let dataProducto = productos.find(productoSeleccionado)
 
-    dataProducto.tallaSeleccionada = "xl"
+    dataProducto.tallaSeleccionada = dataProducto.tallas[0]
 
     return {
       imagenSeleccionada: 0,
@@ -135,9 +135,21 @@ export default {
       .tallas-producto {
           font-size: 18px;
           font-weight: bold;
+
           li {
             display: inline;
             margin: auto;
+
+            .boton-talla {
+              background-color: white;
+              border: none;
+              margin: 10px;
+              border-radius: 4px;
+
+              &.seleccionada {
+                background-color: #007bff;
+              }
+            }
           }
         }
     }
