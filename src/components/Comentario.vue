424f54312fc895476760aac8f73b7f25d275dd4b
IsraPerez98 <isra1998isra@gmail.com>
2020-12-21 17:58:27 -0500

    componente comentario

--- src/components/Comentario.vue
+++ src/components/Comentario.vue
@@ -1,6 +1,7 @@
 <template>
-    <b-container v-if="comentario.lenght>0" class="comentario">
+    <b-container  class="comentario">
         <h1 class="titulo">Deja tu comentario</h1>
         <b-container id="comentario">
+            
             
 </template>
